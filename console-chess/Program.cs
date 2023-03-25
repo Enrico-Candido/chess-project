22ff85df766944d5908a734fd21a0df0d8be36fc
Enrico Candido <enrico.enr07@gmail.com>
2023-03-25 20:08:49 -0300

    Aprimorando o tabuleiro

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -6,9 +6,16 @@ namespace console_chess {
         static void Main(string[] args) {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            try {
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(4, 5));
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
