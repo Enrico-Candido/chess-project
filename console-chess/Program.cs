fc336dac1b9ae863d2fdc3ea4420c3ac41e79d82
Enrico Candido <enrico.enr07@gmail.com>
2023-03-25 14:28:39 -0300

    Implementacao inicial do tabuleiro

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -3,8 +3,7 @@
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Posicao P = new Posicao(3, 4);
-            Console.WriteLine("Posicao " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
         }
     }
 }
