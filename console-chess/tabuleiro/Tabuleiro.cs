6b1d72d9e97fae2b478cd6d4edf2366e0de0bbdc
Enrico Candido <enrico.enr07@gmail.com>
2023-03-25 16:06:08 -0300

    Inicializando excecao personalizada

--- console-chess/tabuleiro/Tabuleiro.cs
+++ console-chess/tabuleiro/Tabuleiro.cs
@@ -4,7 +4,7 @@
         public int colunas { get; set; }
         private Peca[,] pecas;
 
-        public Tabuleiro(int linhas, int colunas) { 
+        public Tabuleiro(int linhas, int colunas) {
             this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
@@ -12,10 +12,27 @@
         public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
         }
-
+        public Peca peca(Posicao pos) {
+            return pecas[pos.linha, pos.coluna];
+        }
+        public bool existePeca(Posicao pos) {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
         public void colocarPeca(Peca p, Posicao pos) {
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+        public bool posicaoValida(Posicao pos) {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas) {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Posicao pos) {
+            if (!posicaoValida(pos)) {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
---
+++ console-chess/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,6 @@
+﻿namespace tabuleiro {
+    class TabuleiroException : Exception {
+        public TabuleiroException(string msg) : base(msg) {
+        }
+    }
+}
