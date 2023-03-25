340d63adca6f2925f676d965c2b28b473260a8b1
Enrico Candido <enrico.enr07@gmail.com>
2023-03-25 15:00:13 -0300

    Implementando o metodo de colocar pecas

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,10 +1,16 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace console_chess {
     class Program {
         static void Main(string[] args) {
             Tabuleiro tab = new Tabuleiro(8, 8);
-            
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
+            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(4, 5));
+
             Tela.imprimirTabuleiro(tab);
         }
     }
