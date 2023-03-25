3076f44867c242f0186473084b6170e1a9e3d683
Enrico Candido <enrico.enr07@gmail.com>
2023-03-25 12:15:22 -0300

    Criando classe Posicao

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using tabuleiro;
+
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine("Posicao " + P);
+        }
+    }
+}
