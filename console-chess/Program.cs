2a23146213f3c3185c491af07809ac5b4a18aa4c
Enrico Candido <enrico.enr07@gmail.com>
2023-03-26 13:16:15 -0300

    Testes iniciais com movimentos possiveis das pecas

--- console-chess/Program.cs
+++ console-chess/Program.cs
@@ -15,6 +15,14 @@ namespace console_chess {
 
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
+
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
--- console-chess/Tela.cs
+++ console-chess/Tela.cs
@@ -6,18 +6,27 @@ namespace console_chess {
         public static void imprimirTabuleiro(Tabuleiro tab) {
             for (int i = 0; i < tab.linhas; i++) {
                 Console.Write(8 - i + " ");
+                for (int j = 0; j < tab.colunas; j++) imprimirPeca(tab.peca(i, j));
+                Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis) {
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkBlue;
+
+            for (int i = 0; i < tab.linhas; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++) {
-                    if (tab.peca(i, j) == null) {
-                        Console.Write("- ");
-                    }
-                    else {
-                        imprimirPeca(tab.peca(i, j));
-                        Console.Write(" ");
-                    }
+                    if (posicoesPossiveis[i, j]) Console.BackgroundColor = fundoAlterado;
+                    else Console.BackgroundColor = fundoOriginal;
+                    imprimirPeca(tab.peca(i, j));
+                    Console.BackgroundColor = fundoOriginal;
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+            Console.BackgroundColor = fundoOriginal;
         }
         public static PosicaoXadrez lerPosicaoXadrez() {
             string s = Console.ReadLine();
@@ -26,12 +35,17 @@ namespace console_chess {
             return new PosicaoXadrez(coluna, linha);
         }
         public static void imprimirPeca(Peca peca) {
-            if (peca.cor == Cor.Branca) Console.Write(peca);
+            if (peca == null) Console.Write("- ");
             else {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+
+                if (peca.cor == Cor.Branca) Console.Write(peca);
+                else {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
             }
         }
     }
